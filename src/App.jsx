ea95a96fbff921c860362466a7441be51aa2550a
Leya jude <leyajude3@gmail.com>
2024-03-10 17:37:27 +0530

    inital

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Quiz from './Quiz';
+function App() {
+  return (
+    <div className="App">
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
